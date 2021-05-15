d35741ffca2a59ed0cadde1072af2663ebdce63a
Uiop Pilon <Cirutrider21gail.com>
2021-05-14 21:35:26 -0500

    Version 0.1.1 is out!

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,4 +4,14 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [Released]
+### [0.1.1] - 2021-05-14
+#### Added
+- Added a CLI interface using [structopt](https://crates.io/crates/structopt).
+- Added error checking for the CLI interface.
+- Added crates.io integration and uploaded the package to crates.io.
+
+#### Changed
+- Changed the panic profile on release builds to abort (looks more professional).
+
 ## [Unreleased]
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-use structopt::StructOpt;
 use std::path::PathBuf;
 use std::process;
+use structopt::StructOpt;
 
 #[derive(StructOpt, Debug)]
 /// The UNix ARchiver, a simple archive utility compatible with MSVC/GNU/LLVM
-struct CLI {
+struct cli {
     /// Format of generated archive
     #[structopt(short = "f", long = "format", default_value = "gnu")]
     format: String,
@@ -19,7 +19,7 @@ struct CLI {
 }
 
 fn main() {
-    let opt = CLI::from_args();
+    let opt = cli::from_args();
 
     // Check to make sure that the format is one of the following
     // bsd (used by *BSD and darwin), gnu (used by linux and windows) and common (used in .deb packages)
@@ -30,7 +30,7 @@ fn main() {
         _ => {
             eprintln!("error: Invalid Format \"{}\"", opt.format);
             process::exit(1);
-        },
+        }
     }
 
     println!("{:#?}", opt);
